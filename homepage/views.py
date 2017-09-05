68c563892259e2bdc4ab9d0d14196c0729239500
Joe Moorhouse <jbmoorhouse1@gmail.com>
2017-09-05 01:56:24 -0500

    log in from the main page, also minor changes to main page format

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 class Home(generic.TemplateView):
@@ -8,3 +11,13 @@ class Home(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('home')
