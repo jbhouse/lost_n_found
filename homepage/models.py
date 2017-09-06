a9c0aa5530c9a3d7db3422082cf024050e3e642f
Joe Moorhouse <jbmoorhouse1@gmail.com>
2017-09-05 21:40:09 -0500

    created models, and on itial url to test if everything is working correctly

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 # Create your models here.
+
+class FoundItemManager(models.Manager):
+    def create_found_item(self, user):
+        found_item = self.create(user = user)
+        return found_item
+
+class LostItemManager(models.Manager):
+    def create_lost_item(self, user):
+        lost_item = self.create(user = user)
+        return lost_item
+
+class FoundItem(models.Model):
+    description = models.TextField(blank=False)
+    user = models.ForeignKey(User,related_name='founditems')
+    created_at = models.DateTimeField(auto_now=True)
+    latitude = models.IntegerField(blank=False)
+    longitude = models.IntegerField(blank=False)
+    objects = FoundItemManager()
+
+class LostItem(models.Model):
+    description = models.TextField(blank=False)
+    user = models.ForeignKey(User,related_name='lostitems')
+    created_at = models.DateTimeField(auto_now=True)
+    latitude = models.IntegerField(blank=False)
+    longitude = models.IntegerField(blank=False)
+    objects = LostItemManager()
--- homepage/views.py
+++ homepage/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class Home(generic.TemplateView):
@@ -21,3 +22,14 @@ class Home(generic.TemplateView):
             return redirect('home')
         else:
             return redirect('home')
+
+@login_required
+def CreateLostItem(request, **kwargs):
+    if request.method == "POST":
+        response_data = {}
+        if request.user.is_authenticated():
+            user = request.user
+            new_lost_item = LostItem.objects.create_lost_item(user)
+            print('-'*50)
+            print(request.POST)
+            print('-'*50)
